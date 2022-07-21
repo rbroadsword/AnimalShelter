c3de5332c84dd4a3ebadd4e1a3bb8fbd5878e8a2
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-20 20:29:50 -0700

    animalia domas

---
+++ AnimalShelter/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalShelter.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
---
+++ AnimalShelter/Models/animal.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AnimalShelter.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime Date { get; set; }
+        public string Breed { get; set; }
+    }
+}
